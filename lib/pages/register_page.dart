1f045c6b87fca4f3c1898bb386cd2cca4359840b
sachin supunthaka <sachinsupunthaka96@gmail.com>
2022-12-31 18:04:33 +0530

    update; ading for values to their state variables

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mymgstoreapp/pages/login_page.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -8,6 +9,22 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  String _username = '';
+  String _email = '';
+  String _password = '';
+  String _confirmPassword = '';
+
+  final _formKey = GlobalKey<FormState>();
+  void _submit() {
+    final form = _formKey.currentState;
+    if (form!.validate()) {
+      form.save();
+    } else {}
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,138 +42,196 @@ class _RegisterPageState extends State<RegisterPage> {
         child: Center(
           child: SingleChildScrollView(
             child: Form(
+                key: _formKey,
                 child: Column(
-              children: [
-                Text('Register', style: Theme.of(context).textTheme.headline1),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Username',
-                    hintText: 'Enter your username',
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                  children: [
+                    Text('Register',
+                        style: Theme.of(context).textTheme.headline1),
+                    const SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Username',
+                        hintText: 'Enter your username',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        icon: Icon(Icons.person),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightGreen),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null) {
+                          return 'Please enter your username';
+                        }
+                        if (value.isEmpty) {
+                          return 'Please enter your username';
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) => _username = newValue!,
                     ),
-                    icon: Icon(Icons.person),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
+                    const SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Enter your email',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        icon: Icon(Icons.email),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightGreen),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null) {
+                          return 'Please enter your email';
+                        }
+                        if (value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        if (!value.contains('@') == false) {
+                          return 'Please enter a valid email';
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) => _email = newValue!,
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.lightGreen),
+                    const SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        icon: Icon(Icons.lock),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightGreen),
+                        ),
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null) {
+                          return 'Please enter your password';
+                        }
+                        if (value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        if (value.length < 6) {
+                          return 'Password must be at least 6 characters';
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) => _password = newValue!,
                     ),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'Enter your email',
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                    const SizedBox(height: 20.0),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Confirm Password',
+                        hintText: 'Enter your password again',
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        icon: Icon(Icons.lock),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightGreen),
+                        ),
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null) {
+                          return 'Please enter your password again';
+                        }
+                        if (value.isEmpty) {
+                          return 'Please enter your password again';
+                        }
+                        if (value.length < 6) {
+                          return 'Password must be at least 6 characters';
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) => _confirmPassword = newValue!,
                     ),
-                    icon: Icon(Icons.email),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
+                    const SizedBox(height: 20.0),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      child: Text(
+                        'By clicking Register, you agree to our Terms and that you have read our Data Use Policy, including our Cookie Use.',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.lightGreen),
+                    ElevatedButton(
+                      onPressed: _submit,
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.lightGreen,
+                        elevation: 8.0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 50.0, vertical: 15.0),
+                        textStyle: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      child: const Text('Register'),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter your password',
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                    const SizedBox(height: 20.0),
+                    const Text(
+                      'Already have an account?',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    icon: Icon(Icons.lock),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()),
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      child: const Text('Login'),
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.lightGreen),
-                    ),
-                  ),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Confirm Password',
-                    hintText: 'Enter your password again',
-                    labelStyle: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    icon: Icon(Icons.lock),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.lightGreen),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                const Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                  child: Text(
-                    'By clicking Register, you agree to our Terms and that you have read our Data Use Policy, including our Cookie Use.',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.lightGreen,
-                    elevation: 8.0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 50.0, vertical: 15.0),
-                    textStyle: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  child: const Text('Register'),
-                ),
-                const SizedBox(height: 20.0),
-                const Text(
-                  'Already have an account?',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    textStyle: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  child: const Text('Login'),
-                ),
-              ],
-            )),
+                  ],
+                )),
           ),
         ),
       ),
